dae9d4282a0086251454e21be6440a4072252b70
Francisco José García García <frangarcj@gmail.com>
2016-11-08 08:10:24 +0000

    Add windows build

--- build_internal.sh
+++ build_internal.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+    case "${TOXENV}" in
+        WIN)
+	  mkdir build
+	  cd build
+	  cmake .. -DCMAKE_TOOLCHAIN_FILE=toolchain-i686-w64-mingw32.cmake
+	  make tarball
+          ;;
+        *)
+	  mkdir build
+	  cd build
+	  cmake ..
+	  make -j4 tarball
+          ;;
+    esac
 
-mkdir build
-cd build
-cmake .. 
-make -j4 tarball
